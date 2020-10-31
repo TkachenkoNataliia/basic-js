3b0455f5775c700ef901072cf57a564b0b310c02
TkachenkoNataliia <nattaliiatkachenko@gmail.com>
2020-10-31 19:48:14 +0200

    feat: implement function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) === true) {
+    let arrayWithFirstLetters = [];
+    for ( let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        let nameToUpperCase = members[i].toUpperCase();
+        let nameWithoutSpace = nameToUpperCase.split(' ').join('');
+        arrayWithFirstLetters.push(nameWithoutSpace[0]);
+      }
+    }
+    const arrayOfTeam = arrayWithFirstLetters.sort();
+    return arrayOfTeam.join('');
+  } else return false;
 };
